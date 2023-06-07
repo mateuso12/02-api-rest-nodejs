ecaa341b5a1c1eb3c9d0c48d164d07bbcade85fb
mateuso12 <mateuso12@hotmail.com.br>
2023-06-06 22:48:39 -0300

    new: Validando existência do cookie

---
+++ src/middlewares/check-session-id-exists.ts
@@ -0,0 +1,13 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function checkSessionIdExists(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const { sessionId } = request.cookies
+  if (!sessionId) {
+    return reply.code(401).send({
+      error: 'Unauthorized',
+    })
+  }
+}
