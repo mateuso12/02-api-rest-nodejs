64c79638aedc11c868fe37d45cd5867061257569
Mateus Santos <mateuso12@hotmail.com.br>
2023-06-29 17:29:45 -0300

    Finalizando testes da aplicação

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,14 @@
-import { knex as setupKnex, Knex } from 'knex'
-import { env } from './zod'
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL env not found')
-}
+import { Knex, knex as setupKnex } from 'knex'
+import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app'
-import { env } from './zod'
+import { env } from './env'
 
 app
   .listen({
